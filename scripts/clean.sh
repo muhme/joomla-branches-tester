ece9100f7e5d2cc6842ba072508ef8f734f991f8
muhme <github@heikol.de>
2024-06-19 17:31:15 +0200

    Also delete the Docker network with clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #
-# clean.sh - delete all jst_* docker containers
+# clean.sh - delete all jst_* docker containers and the network
+#            (or user docker compose down)
 #
 # MIT License, Copyright (c) 2024 Heiko Lübbe
 # https://github.com/muhme/joomla-system-tests
 
+NETWORK_NAME="joomla-system-tests_default"
+
 source scripts/helper.sh
 
-log 'Removing all joomla system test docker containers jst_*'
+log 'Remove following Joomla System Tests Docker containers'
 docker ps -a --format '{{.Names}}' | grep '^jst_' | xargs -r docker rm -f
+
+log 'Remove following Docker network'
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  docker network rm "$NETWORK_NAME"
+fi
